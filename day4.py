b9b91c0713ddfc6a583187bd2b6883b4e6727c5f
Siva3407 <siva03042007@gmail.com>
2026-07-31 22:09:21 +0530

    Add files via upload

---
+++ day4.py
@@ -0,0 +1,14 @@
+#grade calculator
+a=int(input("enter your marks:"))
+if a>90:
+    print("A+")
+elif a>80:
+    print("A")
+elif a>70:
+    print("B+")
+elif a>60:
+    print("B")
+elif a>50:
+    print("C")
+else:
+    print("fail")
